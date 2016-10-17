328118430171b62cd25c7f8ccfdcb337f6db4871
zqx <zqx@zqx.com>
2016-10-17 21:07:40 +0800

    qqq

--- src/com/app/zhbj/MainActivity.java
+++ src/com/app/zhbj/MainActivity.java
@@ -1,11 +1,24 @@
 package com.app.zhbj;
 
-import android.app.Activity;
-import android.os.Bundle;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends Activity {
+import android.os.Bundle;
+import android.view.Window;
+
+public class MainActivity extends SlidingFragmentActivity {
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);// 去掉标题,
+		// 必须在setContentView之前调用
+		setContentView(R.layout.activity_main);
+
+		// Utils.doSomthing();
+		// R.drawable.p_10
+		setBehindContentView(R.layout.left_menu);
+		SlidingMenu slidingMenu = getSlidingMenu();	
+		slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);// 全屏触摸
+		slidingMenu.setBehindOffset(200);// 屏幕预留200像素宽度
 	}
 }
