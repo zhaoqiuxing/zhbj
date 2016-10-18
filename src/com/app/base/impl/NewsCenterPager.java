7d069bbe7918110008d030d46ad7dd78d9acb35f
zqx <zqx@zqx.com>
2016-10-18 21:49:34 +0800

    111

--- src/com/app/base/impl/NewsCenterPager.java
+++ src/com/app/base/impl/NewsCenterPager.java
@@ -1,9 +1,20 @@
 package com.app.base.impl;
 
 import com.app.base.BasePager;
+import com.app.base.pojo.NewsMenu;
+import com.app.constants.GlobalConstants;
+import com.app.utils.CacheUtils;
+import com.google.gson.Gson;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 
 import android.app.Activity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * 新闻中心
@@ -12,6 +23,7 @@ import android.view.View;
  * @date 2015-10-18
  */
 public class NewsCenterPager extends BasePager {
+	private NewsMenu mNewsData;// 分类信息网络数据
 
 	public NewsCenterPager(Activity activity) {
 		super(activity);
@@ -24,7 +36,50 @@ public class NewsCenterPager extends BasePager {
 		tvTitle.setText("新闻");
 		// 显示菜单按钮
 		btnMenu.setVisibility(View.VISIBLE);
-	}
+		// 先判断有没有缓存,如果有的话,就加载缓存
+		String cache = CacheUtils.getCache(GlobalConstants.CATEGORY_URL, mActivity);
+		if (!TextUtils.isEmpty(cache)) {
+			System.out.println("发现缓存啦...");
+			processData(cache);
+		}
 
+		// 请求服务器,获取数据
+		// 开源框架: XUtils
+		getDataFromServer();
+	}
+	private void processData(String json) {
+		// Gson: Google Json
+		Gson gson = new Gson();
+		mNewsData = gson.fromJson(json, NewsMenu.class);
+		System.out.println("解析结果:" + mNewsData);
+
+	}
+	private void getDataFromServer() {
+		HttpUtils utils = new HttpUtils();
+		utils.send(HttpMethod.GET, GlobalConstants.CATEGORY_URL, new RequestCallBack<String>() {
+
+			@Override
+			public void onSuccess(ResponseInfo<String> responseInfo) {
+				// 请求成功
+				String result = responseInfo.result;// 获取服务器返回结果
+				System.out.println("服务器返回结果:" + result);
+				// JsonObject, Gson
+				processData(result);
+
+				// 写缓存
+				CacheUtils.setCache(GlobalConstants.CATEGORY_URL, result, mActivity);
+
+			}
+
+			@Override
+			public void onFailure(HttpException error, String msg) {
+				// 请求失败
+				error.printStackTrace();
+				Toast.makeText(mActivity, msg, Toast.LENGTH_SHORT).show();
+			}
+
+		});
+
+	}
 
 }
--- src/com/app/fragment/ContentFragment.java
+++ src/com/app/fragment/ContentFragment.java
@@ -30,6 +30,7 @@ public class ContentFragment extends BaseFragment {
 	private NoScrollViewPager mViewPager;
 	private RadioGroup rgGroup;
 	private ArrayList<BasePager> mPagers;// 五个标签页的集合
+
 	@Override
 	public View initView() {
 		View view = View.inflate(mActivity, R.layout.fragment_content, null);
@@ -51,79 +52,86 @@ public class ContentFragment extends BaseFragment {
 
 		mViewPager.setAdapter(new ContentAdapter());
 		// 底栏标签切换监听
-				rgGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+		rgGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-					@Override
-					public void onCheckedChanged(RadioGroup group, int checkedId) {
-						switch (checkedId) {
-						case R.id.rb_home:
-							// 首页
-							// mViewPager.setCurrentItem(0);
-							mViewPager.setCurrentItem(0, false);// 参2:表示是否具有滑动动画
-							break;
-						case R.id.rb_news:
-							// 新闻中心
-							mViewPager.setCurrentItem(1, false);
-							break;
-						case R.id.rb_smart:
-							// 智慧服务
-							mViewPager.setCurrentItem(2, false);
-							break;
-						case R.id.rb_gov:
-							// 政务
-							mViewPager.setCurrentItem(3, false);
-							break;
-						case R.id.rb_setting:
-							// 设置
-							mViewPager.setCurrentItem(4, false);
-							break;
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				switch (checkedId) {
+				case R.id.rb_home:
+					// 首页
+					// mViewPager.setCurrentItem(0);
+					mViewPager.setCurrentItem(0, false);// 参2:表示是否具有滑动动画
+					break;
+				case R.id.rb_news:
+					// 新闻中心
+					mViewPager.setCurrentItem(1, false);
+					break;
+				case R.id.rb_smart:
+					// 智慧服务
+					mViewPager.setCurrentItem(2, false);
+					break;
+				case R.id.rb_gov:
+					// 政务
+					mViewPager.setCurrentItem(3, false);
+					break;
+				case R.id.rb_setting:
+					// 设置
+					mViewPager.setCurrentItem(4, false);
+					break;
 
-						default:
-							break;
-						}
-					}
-				});
-				
-				mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
-
-					@Override
-					public void onPageSelected(int position) {
-						BasePager pager = mPagers.get(position);
-						pager.initData();
-					}
-
-					@Override
-					public void onPageScrolled(int position, float positionOffset,
-							int positionOffsetPixels) {
-
-					}
-
-					@Override
-					public void onPageScrollStateChanged(int state) {
-
-					}
-				});
-				// 手动加载第一页数据
-				mPagers.get(0).initData();
-				// 首页禁用侧边栏
-				setSlidingMenuEnable(false);
-			}
-
-			/**
-			 * 开启或禁用侧边栏
-			 * 
-			 * @param enable
-			 */
-			protected void setSlidingMenuEnable(boolean enable) {
-				// 获取侧边栏对象
-				MainActivity mainUI = (MainActivity) mActivity;
-				SlidingMenu slidingMenu = mainUI.getSlidingMenu();
-				if (enable) {
-					slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-				} else {
-					slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+				default:
+					break;
 				}
 			}
+		});
+
+		mViewPager.setOnPageChangeListener(new OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int position) {
+				BasePager pager = mPagers.get(position);
+				pager.initData();
+
+				if (position == 0 || position == mPagers.size() - 1) {
+					// 首页和设置页要禁用侧边栏
+					setSlidingMenuEnable(false);
+				} else {
+					// 其他页面开启侧边栏
+					setSlidingMenuEnable(true);
+				}
+			}
+
+			@Override
+			public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int state) {
+
+			}
+		});
+		// 手动加载第一页数据
+		mPagers.get(0).initData();
+		// 首页禁用侧边栏
+		setSlidingMenuEnable(false);
+	}
+
+	/**
+	 * 开启或禁用侧边栏
+	 * 
+	 * @param enable
+	 */
+	protected void setSlidingMenuEnable(boolean enable) {
+		// 获取侧边栏对象
+		MainActivity mainUI = (MainActivity) mActivity;
+		SlidingMenu slidingMenu = mainUI.getSlidingMenu();
+		if (enable) {
+			slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		} else {
+			slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+		}
+	}
 
 	class ContentAdapter extends PagerAdapter {
 
@@ -142,7 +150,7 @@ public class ContentFragment extends BaseFragment {
 			BasePager pager = mPagers.get(position);
 			View view = pager.mRootView;// 获取当前页面对象的布局
 
-			 // pager.initData();// 初始化数据, viewpager会默认加载下一个页面,
+			// pager.initData();// 初始化数据, viewpager会默认加载下一个页面,
 			// 为了节省流量和性能,不要在此处调用初始化数据的方法
 
 			container.addView(view);
